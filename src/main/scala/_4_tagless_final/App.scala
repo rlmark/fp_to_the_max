5d80e5346722a88c67425e450405890d7df06533
rmark <rmark@marchex.com>
2018-10-03 16:56:06 -0700

    TAGLESS FINAL IMPLEMENTATION

--- src/main/scala/_4_tagless_final/App.scala
+++ src/main/scala/_4_tagless_final/App.scala
@@ -1,7 +1,9 @@
 package _4_tagless_final
 
-import _4_tagless_final.IO.pure
+import scala.io.StdIn
+import scala.util.Try
 
+// Sequential composition effect
 trait Program[F[_]] {
   def finish[A](a: => A): F[A]
 
@@ -29,30 +31,7 @@ object Program {
   }
 }
 
-case class IO[A](unsafeRun: () => A) { self =>
-  def map[B](f: A => B): IO[B] = {
-    IO[B](() => f(self.unsafeRun()))
-  }
-
-  def flatMap[B](f: A => IO[B]): IO[B] = {
-    /* note: while this flatMap implementation compiles: f(this.unsafeRun), this is evaluated immediately.
-    This doesn't give us the ability to separate program definition from its execution. */
-    IO[B](() => f(self.unsafeRun()).unsafeRun())
-  }
-}
-object IO {
-  def pure[A](a: A): IO[A] = IO(() => a)
-}
-object IOHelpers {
-  implicit val ioProgram: Program[IO] = new Program[IO] {
-    override def finish[A](a: => A): IO[A] = pure(a)
-
-    override def chain[A, B](fa: IO[A], atb: A => IO[B]): IO[B] = fa.flatMap(atb)
-
-    override def map[A, B](fa: IO[A], f: A => B): IO[B] = fa.map(f)
-  }
-}
-
+// Effect for printing / reading
 trait Console[F[_]] {
   def putStrLine(s: String):F[Unit]
 
@@ -66,6 +45,7 @@ object ConsoleHelpers {
   def getLine[F[_]: Console](): F[String] = Console[F].getStrLine()
 }
 
+// Effect for creating random numbers
 trait Random[F[_]] {
   def nextInt(upper: Int): F[Int]
 }
@@ -76,59 +56,89 @@ object RandomHelpers {
   def nextInt[F[_]](upper: Int)(implicit F: Random[F]): F[Int] = Random[F].nextInt(upper)
 }
 
-object App {
-  def main[F[_]: Program](args: Array[String]): Unit = {
+case class IO[A](unsafeRun: () => A) { self =>
+  def map[B](f: A => B): IO[B] = {
+    IO[B](() => f(self.unsafeRun()))
+  }
+
+  def flatMap[B](f: A => IO[B]): IO[B] = {
+    /* note: while this flatMap implementation compiles: f(this.unsafeRun), this is evaluated immediately.
+    This doesn't give us the ability to separate program definition from its execution. */
+    IO[B](() => f(self.unsafeRun()).unsafeRun())
+  }
+}
+
+object IO {
+  def pure[A](a: A): IO[A] = IO(() => a)
+
+  implicit val ioProgram: Program[IO] = new Program[IO] {
+    override def finish[A](a: => A): IO[A] = pure(a)
+
+    override def chain[A, B](fa: IO[A], atb: A => IO[B]): IO[B] = fa.flatMap(atb)
+
+    override def map[A, B](fa: IO[A], f: A => B): IO[B] = fa.map(f)
+  }
+
+  implicit val consoleIO: Console[IO] = new Console[IO] {
+    override def putStrLine(s: String): IO[Unit] = IO(() => println(s))
+
+    override def getStrLine(): IO[String] = IO(() => StdIn.readLine())
+  }
+
+  implicit val randomIO: Random[IO] = new Random[IO] {
+    override def nextInt(upper: Int): IO[Int] =  IO(() => scala.util.Random.nextInt(upper) + 1)
+  }
+}
+
+object Main {
+  import Program._
+  import ConsoleHelpers._
+  import RandomHelpers._
+
+  def main(args: Array[String]): Unit = {
+    main[IO](args).unsafeRun()
+  }
+
+  def main[F[_]: Console: Random: Program](args: Array[String]): F[Unit] = {
     val program = for {
-      _ <- printlnIO("Hey there, let's play a guessing game. What is your name?")
-      name <- readlnIO()
-      _ <- printlnIO(s"Great, $name, let's begin")
+      _ <- putLine("Hey there, let's play a guessing game. What is your name?")
+      name <- getLine()
+      _ <- putLine(s"Great, $name, let's begin")
       _ <- gameLoop(name)
     } yield ()
 
-    program.unsafeRun()
-  }
-
-  def printlnIO(s: String): IO[Unit] = {
-    IO(() => println(s))
-  }
-
-  def readlnIO(): IO[String] = {
-    IO(() => StdIn.readLine())
+    program
   }
 
   def parseInt(s: String): Option[Int] = {
     Try(s.toInt).toOption
   }
 
-  def generateNumber(upper: Int): IO[Int] = {
-    IO(() => Random.nextInt(upper) + 1)
-  }
-
-  def gameLoop(name: String): IO[Unit] = {
+  def gameLoop[F[_]: Program :Random :Console](name: String): F[Unit] = {
     for {
-      _ <- printlnIO("I'm thinking of a number between 1 and 5. What's your guess")
-      targetNum <- generateNumber(5)
-      playerGuess <- readlnIO()
+      _ <- putLine("I'm thinking of a number between 1 and 5. What's your guess")
+      targetNum <- nextInt(5)
+      playerGuess <- getLine()
       _ <- scoreGame(parseInt(playerGuess), targetNum, name)
       continue <- checkPlayerContinue()
-      _ <- if(continue) gameLoop(name) else IO.pure(())
+      _ <- if(continue) gameLoop(name) else Program[F].finish(())
     } yield ()
   }
 
-  def scoreGame(maybeGuess: Option[Int], targetNum: Int, name: String): IO[Unit] = {
+  def scoreGame[F[_]: Program :Random :Console](maybeGuess: Option[Int], targetNum: Int, name: String): F[Unit] = {
     maybeGuess match {
-      case None => printlnIO("Please enter a number.")
+      case None => putLine("Please enter a number.")
       case Some(int) =>
         if (targetNum == int) {
-          printlnIO(s"Good job, $name, you win!")
-        } else printlnIO(s"Sorry! My number was $targetNum")
+          putLine(s"Good job, $name, you win!")
+        } else putLine(s"Sorry! My number was $targetNum")
     }
   }
 
-  def checkPlayerContinue(): IO[Boolean] = {
+  def checkPlayerContinue[F[_]: Program :Console](): F[Boolean] = {
     for {
-      _ <- printlnIO("Do you want to continue? y/n")
-      playerContinue <- readlnIO()
+      _ <- putLine("Do you want to continue? y/n")
+      playerContinue <- getLine()
       continue = playerContinue match {
         case "y" => true
         case "n" => false
